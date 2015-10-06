a47f1356b60ead0f395930509c235e1205bb26d3
tushar8590 <tushar8590@gmail.com>
2015-10-06 07:59:10 +0530

    updated MSP URL Extractor

--- SeliniumDemo/src/com/dataUpdater/bl/CssTester.java
+++ SeliniumDemo/src/com/dataUpdater/bl/CssTester.java
@@ -4,14 +4,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class CssTester {
-    
+   static String productUrl;
     public static void main(String[] args) {
         HtmlUnitDriver driver = new HtmlUnitDriver();
-       driver.get("http://www.mysmartprice.com/mobile/pricelist/mobile-price-list-in-india.html#subcategory=mobile");
+       /*driver.get("http://www.mysmartprice.com/mobile/pricelist/mobile-price-list-in-india.html#subcategory=mobile");
        System.out.println("Name " +driver.findElement(By.cssSelector("#msp_body > div > div.msplistleft > div.productlistmiddle > div.listitems_rd.info-items-5 > div.product-list > div:nth-child(5) > div > a")).getText() );
       // System.out.println("Price " +driver.findElement(By.id("priceblock_ourprice")).getText() );
-       
-        
+       */
+        driver.get("http://www.mysmartprice.com/mobile/pricelist/pages/smart-watches-price-list-in-india-2.html");
+        for(int i = 4 ;i<=51;i++){
+            System.out.println("Count = " + i);
+            productUrl = driver.findElement(By.xpath("//*[@id='msp_body']/div/div[1]/div[4]/div[1]/div[3]/div["+i+"]/div/a")).getAttribute("href");            										   //*[@id="msp_body"]/div/div[1]/div[5]/div[1]/div[4]/div[5]/div/a
+                                                       //*[@id="msp_body"]/div/div[1]/div[5]/div[1]/div[4]/div[6]/div/a
+                                                       
+            System.out.println(productUrl);
+          //
+            //this.saveData(productUrl, section);
+            
+        }
+        driver.close();
     }
     
 }
--- SeliniumDemo/src/com/dataUpdater/bl/MSPUrlExtractor.java
+++ SeliniumDemo/src/com/dataUpdater/bl/MSPUrlExtractor.java
@@ -14,6 +14,8 @@ import java.util.logging.Level;
 import org.openqa.selenium.By;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
+import com.dataLoader.dao.JDBCConnection;
+import com.dataLoader.dao.SQLQueries;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 
 public class MSPUrlExtractor {
@@ -26,12 +28,12 @@ public class MSPUrlExtractor {
 	static Map<String,List<String>> urlMap;
 	public static void main(String[] args) {
 		MSPUrlExtractor msp = new MSPUrlExtractor();
-		ExecutorService executor = Executors.newFixedThreadPool(2);
+		ExecutorService executor = Executors.newFixedThreadPool(3);
 		  List<Future<String>> list = new ArrayList<Future<String>>();
 		urlMap = new HashMap<String,List<String>>();
 		urlMap.put("mobiles", Arrays.asList("http://www.mysmartprice.com/mobile/pricelist/mobile-price-list-in-india.html#subcategory=mobile","http://www.mysmartprice.com/mobile/pricelist/pages/mobile-price-list-in-india-","76"));
-		//urlMap.put("tablets", Arrays.asList("http://www.mysmartprice.com/mobile/pricelist/tablet-price-list-in-india.html#subcategory=tablet&property=1200652-2267506","http://www.mysmartprice.com/mobile/pricelist/pages/tablet-price-list-in-india-2.html"));
-		//urlMap.put("smatwatches", Arrays.asList("http://www.mysmartprice.com/mobile/pricelist/smart-watches-price-list-in-india.html","http://www.mysmartprice.com/mobile/pricelist/pages/smart-watches-price-list-in-india-2.html"));
+		urlMap.put("tablets", Arrays.asList("http://www.mysmartprice.com/mobile/pricelist/tablet-price-list-in-india.html#subcategory=tablet&property=1200652-2267506","http://www.mysmartprice.com/mobile/pricelist/pages/tablet-price-list-in-india-2.html","11"));
+		urlMap.put("smatwatches", Arrays.asList("http://www.mysmartprice.com/mobile/pricelist/smart-watches-price-list-in-india.html","http://www.mysmartprice.com/mobile/pricelist/pages/smart-watches-price-list-in-india-2.html","4"));
 		urlMap.forEach((k,v)->{
 			Callable<String> callable = msp.new  DataExtractor(v.get(0),v.get(1),v.get(2),k);
 			Future<String> future = executor.submit(callable);
@@ -50,41 +52,64 @@ public class MSPUrlExtractor {
 		HtmlUnitDriver driver;
 		 String section;
 		 String keyword; //2.html
+		 String query;
+		 List<String> params;
+		 JDBCConnection conn;
    	 public DataExtractor(String baseUrl,String otherUrl,String limit,String section){
    		this.baseUrl = baseUrl;
    		this.otherUrls = otherUrl;
    		this.section = section;
    		this.limit = Integer.parseInt(limit);
    		driver = new HtmlUnitDriver(BrowserVersion.CHROME);
-   		driver.setJavascriptEnabled(true);
+   		params = new ArrayList<>();
+   		conn = JDBCConnection.getInstance();
+   		//driver.setJavascriptEnabled(true);
    	 }
 	@Override
 	public Object call() throws Exception {
-	System.out.println("Calling  for "+section+" "+ baseUrl +"  " +otherUrls); 
+	System.out.println("Calling  for "+section); 
 	   // get the base url data  
 	        try{
 	        driver.get(baseUrl);
-	        System.out.println("M here");
 	        for(int i = 4 ;i<=51;i++){
-	            System.out.println("M here");
-	            productUrl = driver.findElement(By.cssSelector("#msp_body > div > div.msplistleft > div.productlistmiddle > div.listitems_rd.info-items-5 > div.product-list > div:nth-child("+i+") > a")).getAttribute("href");
+	        	if(section.equals("tablets"))
+	        		productUrl = driver.findElement(By.xpath("//*[@id='msp_body']/div/div[1]/div[5]/div[1]/div[4]/div["+i+"]/div/a")).getAttribute("href");
+	        	else
+	                productUrl = driver.findElement(By.xpath("//*[@id='msp_body']/div/div[1]/div[4]/div[1]/div[3]/div["+i+"]/div/a")).getAttribute("href");
 	            this.saveData(productUrl, section);
 	        }
 	        
-	        }catch(Exception e){
-	            e.printStackTrace();
-	        } 
 	        
-	 /* for(int i = 2;i<=limit;i++){
-	      
-	  }*/
-	    
+	    // for the otherUrls    
+	  for(int j = 2;j<=limit;j++){
+		 
+		  driver.get(otherUrls+j+".html");
+	        for(int i = 4 ;i<=51;i++){
+	        	if(section.equals("tablets"))
+	        		productUrl = driver.findElement(By.xpath("//*[@id='msp_body']/div/div[1]/div[4]/div[1]/div[4]/div["+i+"]/div/a")).getAttribute("href");
+	        	else
+	                productUrl = driver.findElement(By.xpath("//*[@id='msp_body']/div/div[1]/div[4]/div[1]/div[3]/div["+i+"]/div/a")).getAttribute("href");
+	            this.saveData(productUrl, section);
+	        }
+	  }
+	
+	  
+	        }catch(Exception e){
+	        	System.out.println("Section " + section);
+	        	e.getMessage();
+	            e.printStackTrace();
+	        }  
 	    driver.close();
 		return null;
 	}
 	
 	private void saveData(String url,String section){
-	    System.out.println(url+" "+ section);
+		 query = SQLQueries.insertMspProductUrl;
+		 params.add(url);
+		 params.add(section);
+		 conn.upsertData(query, params); 
+		 params.clear();
+	   // System.out.println(url+" "+ section);
 	}
    	 
 	}
