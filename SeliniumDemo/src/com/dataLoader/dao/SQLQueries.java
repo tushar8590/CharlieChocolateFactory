4f94b37e8ff31186e9eaad981e9b148384d83c84
tushar8590 <tushar8590@gmail.com>
2015-10-09 08:21:52 +0530

    data changes

--- SeliniumDemo/src/com/dataLoader/dao/SQLQueries.java
+++ SeliniumDemo/src/com/dataLoader/dao/SQLQueries.java
@@ -83,5 +83,6 @@ public class SQLQueries {
 	   // 1sth October 2015
 	   public static String  insertMspProductUrl = "insert into msp_product_url(url,section) values(?,?)  ";
 	   public static String insertElectronicData = "insert into sp_electronics(model,price,url,image_url,section,mapped_flag) values()";
-	   public static String getMspUrls  = "SELECT * FROM `msp_product_url`"; 
+	   public static String getMspUrls  = "SELECT * FROM `msp_product_url` LIMIT 10"; 
+	   public static String insertMspVendorUrl = "insert into msp_electronics(model,url,vendor) values (?,?,?)";
 }
--- SeliniumDemo/src/com/dataUpdater/bl/MSPCatDataExtractor.java
+++ SeliniumDemo/src/com/dataUpdater/bl/MSPCatDataExtractor.java
@@ -3,12 +3,15 @@ package com.dataUpdater.bl;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import org.apache.commons.collections.map.HashedMap;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -20,7 +23,7 @@ import com.dataLoader.dao.SQLQueries;
 public class MSPCatDataExtractor {
 
 	 JDBCConnection conn;
-	 
+	 Map<String,String> urlMap;
 	 
 	public static void main(String[] args) {
 		MSPCatDataExtractor mspcatDe = new MSPCatDataExtractor();
@@ -44,16 +47,28 @@ public class MSPCatDataExtractor {
 		conn = JDBCConnection.getInstance();
 		String query  = SQLQueries.getMspUrls;
 		ResultSet rs = conn.executeQuery(query, null);
+		WebDriver driver = new FirefoxDriver();
+		urlMap = new HashMap<>();
 		try {
 			while(rs.next()){
 				//System.out.println(rs.getString("url") + "  "+rs.getString("section"));
-				DataExtractor de = 	this.new DataExtractor(rs.getString("url"),rs.getString("section"));
-				 
+				urlMap.put(rs.getString("url"), rs.getString("section"));
+				
 			}
-		} catch (SQLException e) {
+			urlMap.forEach((k,v) -> {
+				DataExtractor de = 	this.new DataExtractor(k,v,driver);
+				 try {
+					de.call();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			});
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally{
+			driver.close();
 			conn.closeConnection();
 		}
 	}
@@ -64,27 +79,43 @@ public class MSPCatDataExtractor {
 		 JDBCConnection conn;
 		 String url;
 		 String section;
-	   	 public DataExtractor(String baseUrl,String section){
+		 WebDriver driver ;
+	   	 public DataExtractor(String baseUrl,String section,WebDriver driver){
 	   		 this.url  = baseUrl;
 	   		 this.section = section;
+	   		 this.driver = driver;
+	   		params = new ArrayList<>();
+	   		conn = JDBCConnection.getInstance();
 	   	 }
 		@Override
 		public Object call() throws Exception {
+			try{
 			// get to the page
-			WebDriver driver = new FirefoxDriver();
 			driver.get(this.url);
-			WebElement radioButton = driver.findElement(By.id("online_stores"));
+			try{
+				WebElement radioButton = driver.findElement(By.xpath("//*[@id='online_stores']"));
+			
 			radioButton.click();
+			}catch(Exception e){}
+			String vendorUrl;
 			for(int i =3;i<=13;i++){
-				System.out.println(driver.findElement(By.xpath("//*[@id='pricetable']/div["+i+"]/div[2]/div[5]/div[2]/div")).getAttribute("data-url"));
+				//System.out.println(driver.findElement(By.xpath("//*[@id='pricetable']/div["+i+"]/div[2]/div[5]/div[2]/div")).getAttribute("data-url"));
+				vendorUrl = driver.findElement(By.xpath("//*[@id='pricetable']/div["+i+"]/div[2]/div[5]/div[2]/div")).getAttribute("data-url");
+				//System.out.println("URL = "+vendorUrl);
+				this.saveData(vendorUrl);
 			}
-			driver.close();
+			
+			}catch(Exception e){
+				//e.printStackTrace();
+				}
+			
 			return null;
 		}
-		private void saveData(String url,String section){
-			 query = SQLQueries.insertMspProductUrl;
-			 params.add(url);
-			 params.add(section);
+		private void saveData(String vendorUrl){
+			 query = SQLQueries.insertMspVendorUrl;
+			 params.add(url.substring(url.lastIndexOf("/")+1,url.length()));
+			 params.add(vendorUrl);
+			 params.add(this.section);
 			 conn.upsertData(query, params); 
 			 params.clear();
 		   // System.out.println(url+" "+ section);
