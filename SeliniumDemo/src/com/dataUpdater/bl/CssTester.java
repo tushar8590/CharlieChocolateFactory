6f4a4eb9df8fb91bd95b767831ca6def7e935618
JN1831 <JN1831@NOIDW7JN1831.snaponglobal.com>
2015-10-10 13:04:06 +0530

    updated

--- SeliniumDemo/src/com/dataUpdater/bl/CssTester.java
+++ SeliniumDemo/src/com/dataUpdater/bl/CssTester.java
@@ -5,23 +5,32 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class CssTester {
    static String productUrl;
+   
     public static void main(String[] args) {
         HtmlUnitDriver driver = new HtmlUnitDriver();
        /*driver.get("http://www.mysmartprice.com/mobile/pricelist/mobile-price-list-in-india.html#subcategory=mobile");
        System.out.println("Name " +driver.findElement(By.cssSelector("#msp_body > div > div.msplistleft > div.productlistmiddle > div.listitems_rd.info-items-5 > div.product-list > div:nth-child(5) > div > a")).getText() );
       // System.out.println("Price " +driver.findElement(By.id("priceblock_ourprice")).getText() );
        */
-        driver.get("http://www.mysmartprice.com/mobile/pricelist/pages/smart-watches-price-list-in-india-2.html");
-        for(int i = 4 ;i<=51;i++){
-            System.out.println("Count = " + i);
-            productUrl = driver.findElement(By.xpath("//*[@id='msp_body']/div/div[1]/div[4]/div[1]/div[3]/div["+i+"]/div/a")).getAttribute("href");            										   //*[@id="msp_body"]/div/div[1]/div[5]/div[1]/div[4]/div[5]/div/a
-                                                       //*[@id="msp_body"]/div/div[1]/div[5]/div[1]/div[4]/div[6]/div/a
-                                                       
-            System.out.println(productUrl);
-          //
+		 String deliveryTime;
+		 String emi;
+		 String cod;
+		 String rating;
+        
+        driver.get("http://www.mysmartprice.com/mobile/apple-iphone-6-msp4340");
+        // image 
+        System.out.println(driver.findElement(By.xpath("//*[@id='mspSingleImg']")).getAttribute("src"));
+        // delivery
+        System.out.println(driver.findElement(By.xpath("//*[@id='pricetable']/div[3]/div[2]/div[4]/div[1]")).getText());
+        //price
+        System.out.println(driver.findElement(By.xpath("//*[@id='pricetable']/div[3]/div[2]/div[5]/div[1]/div[1]")).getText());
+        // emi
+        System.out.println(driver.findElement(By.xpath("//*[@id='pricetable']/div[3]/div[2]/div[3]/div[1]")).getText());
+        //rating
+        System.out.println(driver.findElement(By.xpath("//*[@id='pricetable']/div[3]/div[2]/div[2]/div[2]")).getAttribute("data-callout"));
+        //cod
+        System.out.println(driver.findElement(By.xpath("//*[@id='pricetable']/div[4]/div[2]/div[3]/div[3]")).getAttribute("class"));
             //this.saveData(productUrl, section);
-            
-        }
         driver.close();
     }
     
