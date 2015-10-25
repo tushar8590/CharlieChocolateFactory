7793790599fab660a08d46fd91af896ca1ab2a4f
tushar8590 <tushar8590@gmail.com>
2015-10-25 11:08:55 +0530

    MSPUrlResolver Updare

--- SeliniumDemo/src/com/dataLoader/bl/MspUrlResolver.java
+++ SeliniumDemo/src/com/dataLoader/bl/MspUrlResolver.java
@@ -7,8 +7,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.logging.Level;
 
 import org.openqa.selenium.By;
@@ -20,6 +28,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.dataLoader.dao.JDBCConnection;
 import com.dataLoader.dao.SQLQueries;
+import com.dataUpdater.bl.SelMultiThreadingDemoSpec;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 
 /**
@@ -31,42 +40,168 @@ public class MspUrlResolver {
     
     
     JDBCConnection conn;
-    static String website;
+    
     static{
         java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF); 
         System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
     }
+    Map<String, List<OldUrlMap>> urlMap;
+	static String idBase = "ACNEW";
+	int count;
+	private static Map<String, Map<String, List<OldUrlMap>>> mainMap;
+	private List<OldUrlMap> urlList;
+	
     
-    public void populateList() throws Exception{
+    public static void main(String[] args) {
+    	MspUrlResolver mspSpecExtractor = new MspUrlResolver();
+		mspSpecExtractor.getUrls();
+		HtmlUnitDriver driver = new HtmlUnitDriver(BrowserVersion.FIREFOX_24);
+		ExecutorService executor = Executors.newFixedThreadPool(6);
+		List<Future<String>> list = new ArrayList<Future<String>>();
 
-        int counter = 0;
-        conn = JDBCConnection.getInstance();
-        //ResultSet rs = conn.executeQuery(SQLQueries.getURLsPCIFeed, null);
-        ResultSet rs = conn.executeQuery(SQLQueries.getUnresolvedUrls, null);
-        
-        System.out.println("Starting at "+new Timestamp(new Date().getTime()));
-       
-        List<String> params = new ArrayList<String>();
-        String id = "";
-        
-            while(rs.next()){
-            	HtmlUnitDriver driver = null;
-                try {
-                	  driver = new HtmlUnitDriver(BrowserVersion.FIREFOX_24);
-                    String urlOld = rs.getString("url");
-                    MspUrlResolver.website = rs.getString("website");
-                    id = rs.getString("id");
-                  //  HtmlUnitDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME);
-                    driver.get(urlOld);
-                   
-                    (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+		for(Map.Entry<String,Map<String,List<OldUrlMap>>> topLevelMenuMapEntry:mainMap.entrySet()){
+
+		    for(Map.Entry<String, List<OldUrlMap>> sectionLevelEntry:topLevelMenuMapEntry.getValue().entrySet() ){
+
+				Callable<String> callable = mspSpecExtractor.new DataExtractor(sectionLevelEntry.getValue(),sectionLevelEntry.getKey(), driver, idBase);
+				
+				Future<String> future = executor.submit(callable);
+				list.add(future);
+		    }
+			
+		
+		}
+		
+		System.out.println("Shutting down ececutor");
+
+    }
+    
+    
+    
+    
+    public void getUrls() {
+	    System.out.println("Starting at "+new Timestamp(new Date().getTime()));
+		conn = JDBCConnection.getInstance();
+		String mainQuery = "select distinct menu_level1 from msp_electronics where url_mapped='F'";
+		ResultSet rs1 = conn.executeQuery(mainQuery, null);
+
+		mainMap = new HashMap<>();
+
+		try {
+			while (rs1.next()) {
+				String subMenuQuery = "SELECT menu_level2 section FROM msp_electronics WHERE  menu_level1 = '"
+						+ rs1.getString("menu_level1") + "' and url_mapped = 'F' ORDER BY menu_level2";
+				// get the prodct corr to each section
+				ResultSet rs = conn.executeQuery(subMenuQuery, null);
+				urlMap = new HashMap<>();
+				while (rs.next()) {
+					String getProductUrl = "select * from msp_electronics where menu_level2 = '"
+							+ rs.getString("menu_level2") + "' and url_mapped = 'F'";
+					ResultSet rsProductUrl = conn.executeQuery(getProductUrl,
+							null);
+					urlList = new ArrayList<>();
+					while (rsProductUrl.next()) {
+						urlList.add(new OldUrlMap(rsProductUrl.getString("id"),rsProductUrl.getString("website") , rsProductUrl.getString("url")));
+					}
+					urlMap.put(rs.getString("menu_level2"), urlList);
+				}
+				mainMap.put(rs1.getString("menu_level1"), urlMap);
+			}
+			System.out.println("Ending  at "+new Timestamp(new Date().getTime()));
+		} catch (Exception e) {
+		    System.out.println("Error at "+new Timestamp(new Date().getTime()));
+			e.getMessage();
+		} finally {
+
+			// conn.closeConnection();
+		}
+	}
+	
+    class OldUrlMap{
+    	String id;
+    	String website;
+    	String oldUrl;
+    	public OldUrlMap(String id,String website,String oldUrl){
+    	  this.id = id; this.website = website; this.oldUrl = oldUrl;	
+    	}
+		public String getId() {
+			return id;
+		}
+		public String getWebsite() {
+			return website;
+		}
+		public String getOldUrl() {
+			return oldUrl;
+		}
+    	
+    	}
+    
+	
+    class DataExtractor implements Callable{
+		String query;
+		List<String> params;
+		JDBCConnection conn;
+		List<OldUrlMap> url;
+		String section;
+		HtmlUnitDriver driver;
+		String vendorUrl;
+		String deliveryTime;
+		String emi;
+		String cod;
+		String rating;
+		String image;
+		String price;
+		String productid;
+
+		// some static variables
+		 String website="";
+	       
+	         String id = "";
+	       
+	         String oldUrl = "";
+	         StringBuilder resolvedUrl = null;
+	         
+	         
+		public DataExtractor(List<OldUrlMap> baseUrl, String section,HtmlUnitDriver driver, String id) {
+			this.url = baseUrl;
+			this.section = section;
+			this.driver = driver;
+			this.productid = id;
+			params = new ArrayList<>();
+			conn = JDBCConnection.getInstance();
+
+		}
+		@Override
+		public Object call() throws Exception {
+			Iterator<OldUrlMap> itr = url.iterator();
+			OldUrlMap currentUrlObject = null;
+	    	 StringBuilder prdSpec = new StringBuilder();
+	         conn = JDBCConnection.getInstance();
+	         
+	         // defining loop variables
+	         params = new ArrayList<String>();
+	         
+	         
+	         
+			while (itr.hasNext()) {
+				
+				currentUrlObject = itr.next();
+				
+				try {
+					driver = new HtmlUnitDriver(BrowserVersion.FIREFOX_24);
+					id = currentUrlObject.getId();
+					oldUrl = currentUrlObject.getOldUrl();
+					website = currentUrlObject.getWebsite();
+					driver.get(oldUrl);
+					
+					(new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
                         public Boolean apply(WebDriver d) {
                             boolean flag = false;
                             //System.out.println(d.getCurrentUrl());
-                            if(d.getCurrentUrl().startsWith("http://www."+MspUrlResolver.website)){
+                            if(d.getCurrentUrl().startsWith("http://www."+website)){
                                 
                                 flag = true;
-                            }else if(d.getCurrentUrl().startsWith("https://"+MspUrlResolver.website)){
+                            }else if(d.getCurrentUrl().startsWith("https://"+website)){
                                 flag = true;
                                 
                             }else
@@ -77,82 +212,54 @@ public class MspUrlResolver {
                         return flag;
                         }
                     });
-                    
-                    
-                    
-                    // save it
-                  //  temp 
-                    StringBuilder resolvedUrl = null;
-                    if(driver.getCurrentUrl().contains("?"))
+					
+					if(driver.getCurrentUrl().contains("?"))
                     	resolvedUrl = new StringBuilder(driver.getCurrentUrl().substring(0, driver.getCurrentUrl().indexOf("?")));
                     else
                     	resolvedUrl =  new StringBuilder(driver.getCurrentUrl());
                     
-                    if(MspUrlResolver.website.equalsIgnoreCase("amazon"))
+                    if(website.equalsIgnoreCase("amazon"))
                         resolvedUrl.append("?tag=aapcompare0f-21");
-                    else if(MspUrlResolver.website.equalsIgnoreCase("flipkart"))
+                    else if(website.equalsIgnoreCase("flipkart"))
                         resolvedUrl.append("?affid=a123pp9aa");
-                    else if(MspUrlResolver.website.equalsIgnoreCase("infibeam"))
+                    else if(website.equalsIgnoreCase("infibeam"))
                         resolvedUrl.append("?trackId=a12");
-                    else if(MspUrlResolver.website.equalsIgnoreCase("snapdeal"))
+                    else if(website.equalsIgnoreCase("snapdeal"))
                         resolvedUrl.append("?aff_id=37358");
-                    else if(MspUrlResolver.website.equalsIgnoreCase("shopclues"))
+                    else if(website.equalsIgnoreCase("shopclues"))
                         resolvedUrl.append("?ID:756");
-                    else if(MspUrlResolver.website.equalsIgnoreCase("indiatimes") || MspUrlResolver.website.equalsIgnoreCase("paytm"))
+                    else if(website.equalsIgnoreCase("indiatimes") || website.equalsIgnoreCase("paytm"))
                         resolvedUrl =   new StringBuilder("http://clk.omgt5.com/?AID=769090&PID=11365&r=").append(resolvedUrl);
-                    
-                    
-                    
+					
+					
                     String updateQUery = SQLQueries.udpateMspUResolvedUrl;
-                   
+                    
                     params.add(resolvedUrl.toString());
                     params.add(id);
                    
-                    if(conn.upsertData(updateQUery, params)){
-                        counter ++;
-                    }
+                    conn.upsertData(updateQUery, params);
                     
-                    params.clear();
+                    params.clear(); resolvedUrl.delete(0, resolvedUrl.length());
                     
 
-                    
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                    deferUpdate(params, id);
-                    continue;
-                }finally{
-                    //conn.closeConnection();
-                	 driver.close(); 
+		    		 
+				} catch (Exception e) {
+					e.getMessage();
+					String updateQUery = SQLQueries.udpateMspUResolvedUrlDeffered;
+					conn.upsertData(updateQUery, Arrays.asList(id));
+					continue;
+					
+					
+				}finally{
+					driver.quit();
+				}
+			}
 
-                }
-                
-                
-            }
-            
-            System.out.println("Ending at "+new Timestamp(new Date().getTime()));
-            System.out.println("Data Inserted for "+counter+"  items");
-        
-           
-    }
+			return null;
 
-    private void deferUpdate(List<String> params, String id) {
-        String updateQUery = SQLQueries.udpateMspUResolvedUrlDeffered;
-        params.add(id);
-        conn.upsertData(updateQUery, params);
-        params.clear();
-    }
-    
-    public static void main(String[] args) {
-        
-        MspUrlResolver mspURL  = new MspUrlResolver();
-        try {
-            mspURL.populateList();
-        }
-        catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-    
+
+		}
+  
+    }    
     
 }
