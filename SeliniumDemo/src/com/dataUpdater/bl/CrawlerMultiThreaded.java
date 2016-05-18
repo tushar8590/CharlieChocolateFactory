16c5484d65a32b54aff755515b0109273aae8e8c
jn1831 <jn1831@NOIDW7JN1831.snaponglobal.com>
2016-05-18 15:00:04 +0530

    crawler changes

--- SeliniumDemo/src/com/dataUpdater/bl/CrawlerMultiThreaded.java
+++ SeliniumDemo/src/com/dataUpdater/bl/CrawlerMultiThreaded.java
@@ -1,6 +1,9 @@
 package com.dataUpdater.bl;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.io.IOException;
 import java.sql.DriverManager;
@@ -18,10 +21,13 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import com.dataLoader.dao.JDBCConnection;
+import com.dataUpdater.bl.SelMultiThreadingDemo.DataExtractor;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 
  
@@ -42,7 +48,8 @@ public class CrawlerMultiThreaded {
       
         // Lets create the object on basis of section and VendirDAta Object
         con = JDBCConnection.getInstance();
-        String query ="SELECT website, url, resolved_url,section FROM msp_electronics WHERE resolved_url is not null and website IN ('shopclues','flipkart','snapdeal','indiatimes','naaptol','saholic','theitdepot') order by section LIMIT 100";
+        
+        String query ="SELECT website, url, resolved_url,section FROM msp_electronics WHERE resolved_url is not null and website IN ('shopclues','flipkart','snapdeal','indiatimes','naaptol','saholic','theitdepot') and section = 'mobiles' order by section LIMIT 100";
         rs = con.executeQuery(query);
         dataMap = new HashMap<String,List<VendorData>>();
         
@@ -56,9 +63,9 @@ public class CrawlerMultiThreaded {
                     section_temp = rs.getString("section");
                     dataMap.put(section_temp, vendorList); 
                     vendorList.clear();
-                    vendorList.add(new VendorData(rs.getString("website"),rs.getString("url")));
+                    vendorList.add(new VendorData(rs.getString("website"),rs.getString("resolved_url")));
                 } else if(section_temp.equals("") || section_temp.equals(rs.getString("section"))){
-                    vendorList.add(new VendorData(rs.getString("website"),rs.getString("url")));
+                    vendorList.add(new VendorData(rs.getString("website"),rs.getString("resolved_url")));
                 }
                 
                 
@@ -72,15 +79,24 @@ public class CrawlerMultiThreaded {
              e.printStackTrace();
         }
         
+        CrawlerMultiThreaded mst = new CrawlerMultiThreaded();
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        List<Future<String>> list = new ArrayList<Future<String>>();
+              
+        for(Map.Entry<String,List<VendorData>> dataEntry : dataMap.entrySet()){
+           List<VendorData>  vd = (List<VendorData>)dataEntry.getValue();
+            Callable<String> callable = mst.new  DataExtractor(vd,dataEntry.getKey());
+            Future<String> future = executor.submit(callable);
+            //add Future to the list, we can get return value using Future
+            list.add(future);
+       
+        }
+       
+        executor.shutdown();
     }
     
     
-    /*private static getUrls(){
-        
-        
-    }*/
-    
-    
+  
     // A callable class to be called from ExecutorService
     class DataExtractor implements Callable{
 
@@ -91,7 +107,29 @@ public class CrawlerMultiThreaded {
         
         @Override
         public Object call() throws Exception {
+         
+            vendorsDataList.forEach(v -> {
+                VendorData obj = v;
+                try {
+                    CrawlerUtil.callProcessMethod(obj.getWebsite(),obj.getUrl(),con);
+                }
+                catch (Exception e) {
+                   System.out.println(e.getMessage());
+                    //e.printStackTrace();
+                }
+            });
               return null;
+        }
+
+
+        /**
+         * @param vendorsDataList
+         * @param section
+         */
+        public DataExtractor(List<VendorData> vendorsDataList, String section) {
+            super();
+            this.vendorsDataList = vendorsDataList;
+            this.section = section;
         }
         
         
@@ -109,7 +147,7 @@ public class CrawlerMultiThreaded {
 class VendorData{
     private String website;
     private String url;
-    
+  
     
     /**
      * @param website
---
+++ SeliniumDemo/src/com/dataUpdater/bl/CrawlerUtil.java
@@ -0,0 +1,294 @@
+package com.dataUpdater.bl;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.dataLoader.bl.URLResolver;
+import com.dataLoader.dao.JDBCConnection;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+
+public class CrawlerUtil {
+    public static void callProcessMethod(String website, String URL, JDBCConnection con) throws SQLException, IOException{
+
+        String URl = URL;
+
+        String storeName = website;
+        String price = "";
+        System.out.println(website);
+        System.out.println(URl);
+        if(storeName.equalsIgnoreCase("snapdeal")){
+            price = processSD(URl);
+        }
+        else if(storeName.equalsIgnoreCase("flipkart"))
+        {
+            price = processFK(URl);
+        }//not working
+        else if(storeName.equalsIgnoreCase("amazon")){
+            price = processAmazon(URl);
+        }
+        else if(storeName.equalsIgnoreCase("shopclues")){
+            price = processShopClues(URl);
+        }
+        else if(storeName.equalsIgnoreCase("infibeam")){
+            price = processInfiBeam(URl);
+        }//not working
+        else if(storeName.equalsIgnoreCase("paytm")) {
+            price = processPaytm(URl);
+        }
+        else if(storeName.equalsIgnoreCase("indiatimes"))
+        {
+            price = processIndiaTimes(URl);
+        }//not working
+        else if(storeName.equalsIgnoreCase("homeshop18")){
+            price = processHomeshop18(URl);
+        }
+        else if(storeName.equalsIgnoreCase("ebay")){
+            price = processEbay(URl);
+        }
+        else if(storeName.equalsIgnoreCase("theitdepot")){
+            price = processTheitdepot(URl);
+        }
+        else if(storeName.equalsIgnoreCase("naaptol")){
+            price = processNaaptol(URl);
+        }
+        else if(storeName.equalsIgnoreCase("saholic")){
+            price = processSaholic(URl);
+        }
+        else if(storeName.equalsIgnoreCase("askmebazaar")){
+            price = processAskmebazaar(URl);
+        }else{
+            
+        }
+          float priceFloat = Float.parseFloat(price);
+        insertUpdatedPrice( priceFloat,  website, URL, con );
+    }
+
+    public static String processSD(String URL) throws SQLException, IOException{
+        Document doc = Jsoup.connect(URL).get();
+       
+        // price of a product
+        
+        String price = "";
+        if(doc.select("div[class=price-dtls]").first().attr("sp").toString()!= null){
+            
+             price = doc.select("div[class=price-dtls]").first().attr("sp").toString(); 
+            }else{
+                price = doc.select("span[class=pdp-e-i-FINAL]").get(0).toString();
+        }
+        // recommended % of a product
+        //spans.add(doc.select("span[class=unitDigit]").get(0));
+        System.out.println(price);
+        return price;
+    }
+
+    public static String processFK(String URL) throws SQLException, IOException{
+        Document doc = Jsoup.connect(URL).get();
+        Elements spans = new Elements();
+
+        // price of a product
+        spans.add(doc.select("span[class=selling-price omniture-field]").get(0));
+        //spans.add(doc.select("span[class=unitDigit]").get(0));
+        String data = spans.text();
+        data = data.replaceAll("[^0-9]", "");
+        System.out.println(data);
+        return data;
+        //<span class="selling-price omniture-field" data-omnifield="eVar48" data-evar48="34999">Rs. 34,999</span>
+    }
+
+    
+            // need to correct this method for id--- wild card needs to be implemented
+    public static String processAmazon(String URL) throws SQLException, IOException{
+        Document doc = Jsoup.connect(URL).get();
+        Elements spans = new Elements();
+        Elements spans1 =  doc.select("span[id=priceblock_ourprice]");
+        // price of a product
+        //spans.add(doc.select("span[id=color_name_0_price]").get(0));
+        //spans.add(doc.select("span[class=unitDigit]").get(0));
+        String data = spans1.text();
+        data = data.replaceAll("[^0-9.]", "");
+        System.out.println(data);
+        return data;
+        //<span id="priceblock_saleprice" class="a-size-medium a-color-price"><span class="currencyINR">&nbsp;&nbsp;</span> 6,999.00</span> }
+    }
+    
+    public static String processShopClues(String URL) throws SQLException, IOException{
+        Document doc = Jsoup.connect(URL).get();
+        Elements spans = new Elements();
+
+        // price of a product
+        String data = doc.select("meta[itemprop=price]").first().attr("content").toString();
+        //spans.add(doc.select("span[class=unitDigit]").get(0));
+        //String data = spans.text();
+        data = data.replaceAll("[^0-9.]", "");
+        System.out.println(data);
+        return data;
+        //<meta itemprop="price" content="2799.00"> 
+        }
+
+     // for iphone its not working fine.
+    public static String processInfiBeam(String URL) throws SQLException, IOException{
+        Document doc = Jsoup.connect(URL).get();
+        Elements spans = new Elements();
+
+        // price of a product
+        //spans.add(doc.select("span[class=price]").get(0));
+        //String data = doc.select("meta[itemprop=price]").first().attr("content").toString();
+        spans.add(doc.select("span[class=price").get(1));
+        String data = spans.text();
+        data = data.replaceAll("[^0-9.]", "");
+        System.out.println(data);
+        return data;
+        //<meta itemprop="price" content="2799.00"> 
+        }
+    
+    //not working
+    public static String processPaytm(String URL) throws SQLException, IOException{
+        Document doc = Jsoup.connect(URL).get();
+        Elements spans = new Elements();
+
+        // price of a product
+        spans.add(doc.select("span[ng-if=!product.product.isOnlyCarCategory]").get(0));
+        String data = spans.text();
+        data = data.replaceAll("[^0-9.]", "");
+        System.out.println(data);
+        return data;
+        //<span ng-if="!product.product.isOnlyCarCategory">Buy for Rs 4,987</span>
+        }
+    
+    public static String processIndiaTimes(String URL) throws SQLException, IOException{
+        HtmlUnitDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME);
+        driver.get(URL);
+        (new WebDriverWait(driver, 5)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                boolean flag = false;
+                //System.out.println(d.getCurrentUrl());
+                if(URL.startsWith("http://www."+"indiatimes")){
+                    
+                    flag = true;
+                }else if(URL.startsWith("https://"+"indiatimes")){
+                    flag = true;
+                    
+                }else
+                {
+                    flag = true;
+                    
+                }
+            return flag;
+            }
+        });
+        Elements spans =  new Elements();
+
+        // price of a product
+        List<WebElement> listTh = driver.findElementsByXPath("//span[contains(@class,'offerprice flt')]");
+        WebElement elem = listTh.get(0);
+        String price = elem.getText().replaceAll("\\D+", "");   
+        
+        return price;
+        //<span ng-if="!product.product.isOnlyCarCategory">Buy for Rs 4,987</span>
+        }
+    
+    
+    //not working
+    public static String processHomeshop18(String URL) throws SQLException, IOException{
+        HtmlUnitDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME);
+        driver.get(URL);
+        List<WebElement> listTh = driver.findElementsByXPath("//span[contains(@id,'hs18Price')]");
+        WebElement elem = listTh.get(0);
+            String price = elem.getText().replaceAll("\\D+", "");           
+            System.out.println(price);
+        
+            return price;
+       
+       // driver.close();
+        
+        }
+    
+    
+    public static String processEbay(String URL) throws SQLException, IOException{
+        Document doc = Jsoup.connect(URL).get();
+        Elements spans = new Elements();
+
+        // price of a product
+        spans.add(doc.select("div[itemprop=price]").get(0));
+        String data = spans.text();
+        data = data.replaceAll("[^0-9]", "");
+        System.out.println(data);
+        return data;
+        //<div class="topPriceRange" itemprop="price"> Rs. 19,699</div>
+        }
+    
+    public static String processTheitdepot(String URL) throws SQLException, IOException{
+        Document doc = Jsoup.connect(URL).get();
+        Elements spans =  new Elements();
+
+        // price of a product
+        spans.add(doc.select("span[class=price]").get(1));
+        String data = spans.text();
+        data = data.replaceAll("[^0-9]", "");
+        System.out.println(data);
+        return data;
+        //<span class="price">Rs.71509/-</span>
+        }
+    
+    
+    public static String processNaaptol(String URL) throws SQLException, IOException{
+        Document doc = Jsoup.connect(URL).get();
+        Elements spans =  new Elements();
+        //Elements spans = doc.select("span[class=offer-price]");
+        // price of a product
+        spans.add(doc.select("span[class=offer-price]").get(0));
+        String data = spans.text();
+        data = data.replaceAll("[^0-9+]", "");
+        data= StringUtils.substringBefore(data, "+");
+        System.out.println(data);
+        return data;
+        //<span class="offer-price"><span class="rs"><!--Rs.--></span>2,499 <span class="ship-price">+ 99 Shipping</span></span>
+        }
+    
+    
+    public static String processSaholic(String URL) throws SQLException, IOException{
+        Document doc = Jsoup.connect(URL).get();
+        Elements spans =  new Elements();
+        //Elements spans = doc.select("span[class=offer-price]");
+        // price of a product
+        spans.add(doc.select("span[class=list-price red bold]").get(0));
+        String data = spans.text();
+        data = data.replaceAll("[^0-9]", "");
+        System.out.println(data);
+        return data;
+        //<span id="sp" class="list-price red bold">11390</span>
+        }
+    
+    public static String processAskmebazaar(String URL) throws SQLException, IOException{
+        Document doc = Jsoup.connect(URL).get();
+        Elements spans =  new Elements();
+        //Elements spans = doc.select("span[class=offer-price]");
+        // price of a product
+        spans.add(doc.select("div[class=price special-price]").get(0));
+        String data = spans.text();
+        data = data.replaceAll("[^0-9.]", "");
+        System.out.println(data);
+        return data;
+        //  <div class="price special-price">Selling Price: <span>8,500.00</span></div>
+        }
+    
+    public static void insertUpdatedPrice(float price, String website, String resolved_url,JDBCConnection con ){
+        Statement stmt1;
+            String Updatequery = " Update msp_electronics set latest_temp_prices = "+price+" where  website = '"+website+"' and resolved_url = '"+resolved_url+"'" ;
+          //  stmt1.executeUpdate(Updatequery);
+            con.upsertData(Updatequery, null);
+        
+    }
+}
